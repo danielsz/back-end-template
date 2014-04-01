2f13f4d8a0c003989bd818eb432211b8b99de4d0
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2014-04-01 20:12:38 +0300

    secure mongo_test

--- src/leiningen/new/back_end_template/mongo_test.clj
+++ src/leiningen/new/back_end_template/mongo_test.clj
@@ -4,11 +4,11 @@
    [com.stuartsierra.component :as component]
    [clojure.test :refer [deftest is]]
    [monger.db :as db]
-   [monger.collection]))
+   [monger.collection]
+   [environ.core :refer [env]]))
 
-(def uri "mongodb://heroku_app11242477:nmtrf5p3goevlaacbjcvlqa419@ds049237.mongolab.com:49237/heroku_app11242477")
 
-(def mongo-db-prod (new-mongo-db uri))
+(def mongo-db-prod (new-mongo-db (env :mongo-url)))
 (def mongo-db-dev (new-mongo-db)) 
 
 (deftest mongo-production
